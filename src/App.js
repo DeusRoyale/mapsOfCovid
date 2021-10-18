7ced27593921663f82a83616820ad2bb015403d1
DeusRoyale <durjoyr369@gmail.com>
2021-10-18 10:03:56 +0600

    2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import "./App.css"
+import React from "react"
+import { MapContainer, TileLayer } from "react-leaflet"
+import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        MapsOfCovid
+        <MapContainer center={[0, 0]} zoom={4}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+        </MapContainer>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
