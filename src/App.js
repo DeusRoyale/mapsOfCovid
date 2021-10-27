cf46c7650b290785f6e9fd0a0f25354d258ddc48
DeusRoyale <durjoyr369@gmail.com>
2021-10-27 07:34:34 +0600

    !!

--- src/App.js
+++ src/App.js
@@ -6,23 +6,33 @@ import teslaData from "./data/teslaSites.json"
 function App() {
   //console.log(teslaData)
 
-  const filteredCountries = teslaData.filter(z => z.address.country === 'Italy')
+  const filteredCountries = teslaData.filter(
+    (z) => z.address.country === "Italy"
+  )
 
   return (
     <div className="App">
       <header className="App-header">
-        <MapContainer center={[41.871941, 12.567380]} zoom={6} scrollWheelZoom={true}>
+        <MapContainer
+          center={[41.871941, 12.56738]}
+          zoom={6}
+          scrollWheelZoom={true}
+        >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {filteredCountries.map((t) => (
-            <Marker
-            key={t.id} 
-            position={[t.gps.latitude, t.gps.longitude]}>
-              {/* <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup> */}
+            <Marker key={t.id} position={[t.gps.latitude, t.gps.longitude]}>
+              {
+                <Popup position={[t.gps.latitude, t.gps.longitude]}>
+                  <div>
+                    <h2>{t.name}</h2>
+                    <p>status: {t.status}</p>
+                    <p>stalls: {t.stallCount}</p>
+                  </div>
+                </Popup>
+              }
             </Marker>
           ))}
         </MapContainer>
