04ce9e235e7d3b24ee49c5c8404f0e665bab9c9a
DeusRoyale <durjoyr369@gmail.com>
2021-10-29 17:05:21 +0600

    and I shall call Him: Adam.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,24 @@
 import "./App.css"
-import React from "react"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import teslaData from "./data/teslaSites.json"
+import React, { useState } from "react"
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+  MapConsumer,
+} from "react-leaflet"
+import { Button } from "react-bootstrap"
+import Covid from "./components/Covid"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 function App() {
-  //console.log(teslaData)
-
-  const filteredCountries = teslaData.filter(
-    (z) => z.address.country === "Italy"
-  )
-
   return (
     <div className="App">
       <header className="App-header">
-        <MapContainer
-          center={[41.871941, 12.56738]}
-          zoom={6}
-          scrollWheelZoom={true}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {filteredCountries.map((t) => (
-            <Marker key={t.id} position={[t.gps.latitude, t.gps.longitude]}>
-              {
-                <Popup position={[t.gps.latitude, t.gps.longitude]}>
-                  <div>
-                    <h2>{t.name}</h2>
-                    <p>status: {t.status}</p>
-                    <p>stalls: {t.stallCount}</p>
-                  </div>
-                </Popup>
-              }
-            </Marker>
-          ))}
-        </MapContainer>
+        <Covid/>
       </header>
     </div>
   )
